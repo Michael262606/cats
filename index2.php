46c11ebcbdfaabfa152d14ba15f91c13f1ac8e7a
Michael262606 <74682538+Michael262606@users.noreply.github.com>
2021-07-25 15:42:52 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -38,7 +38,7 @@
         <h1 href="...">содержания каталога</h1>
         <?php $posts = get_posts($conn); ?>
         <?php foreach($posts as $post) :?>
-          <p> <img src="Images/imag 1.jpg" width="120" height="120" alt="кот"  vspace="5" hspace="5" align="left">
+          <p> <img src="<?php echo $post['Images']?>" width="120" height="120" alt="кот"  vspace="5" hspace="5" align="left">
          <h2> <?php echo $post['title'] ?></h2> 
          <p><?php echo $post['articles'] ?></p>
          <a href="...">читать полностью</a>
